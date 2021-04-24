048a3c22227c63fa7a3a6227c119d22c7d9c03cb
Bad_Sonich <bad.sonich@gmail.com>
2021-04-24 22:49:39 +0300

    добавила стлайдеры

---
+++ imposters.fun/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var slider = $('.projects');
+    var sld = function () {
+        if ($(window).width() < 689) {
+
+            slider.slick({
+                arrows: false,
+                adaptiveHeight: true,
+                speed: 500,
+            });
+        }
+    };
+    sld();
+    $(window).resize(sld);
+    var slider = $('.members');
+    var sld = function () {
+        if ($(window).width() > 689) {
+
+            slider.slick({
+                arrows:true,
+                adaptiveHeight:true,
+                speed:500,
+                slidesToShow:2,
+            });
+        } else {
+
+        }
+    };
+    sld();
+    $(window).resize(sld);
+})
